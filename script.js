d8d5f8c52089857b159bc68b1cb98cc227877655
Priti Munde <120581202+pritimunde1999@users.noreply.github.com>
2023-08-31 22:40:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,43 +1,34 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+  	 const obj = {
+    0: ['M', 1000],
+    1: ['CM', 900],
+    2: ['D', 500],
+    3: ['CD', 400],
+    4: ['C', 100],
+    5: ['XC', 90],
+    6: ['L', 50],
+    7: ['XL', 40],
+    8: ['X', 10],
+    9: ['IX', 9],
+    10: ['V', 5],
+    11: ['IV', 4],
+    12: ['I', 1]
+  };
 
+  let roman = '';
 
-	let romanNumeral = '';
+  for (let i = 0; i < 13; i++) {
+    const symbol = obj[i][0];
+    const value = obj[i][1];
+    const count = Math.floor(num / value);
 
-  //your code here
-// console.log(convertToRoman(36));
-for (let i = 0; i < 7; i++) {
-        const symbol = obj[i][0];
-        const value = obj[i][1];
-
-        const quotient = Math.floor(num / value);
-
-        if (quotient > 0) {
-            if (quotient === 4 && i > 0) {
-                // Handle subtractive notation like IV, XL, CD, etc.
-                romanNumeral += obj[i][0] + obj[i - 1][0];
-                num -= (value - obj[i - 1][1]);
-            } else if (quotient === 9 && i > 0) {
-                // Handle subtractive notation for 9, like IX, XC, CM, etc.
-                romanNumeral += obj[i][0] + obj[i - 2][0];
-                num -= (value - obj[i - 2][1]);
-            } else {
-                romanNumeral += symbol.repeat(quotient);
-                num -= quotient * value;
-            }
-        }
+    if (count > 0) {
+      roman += symbol.repeat(count);
+      num %= value;
     }
+  }
 
-    return romanNumeral;
-
+  return roman;
 
 	
 	
